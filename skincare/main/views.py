e77a61bb071c372cd05a5c7b263ba178c8fb4017
AshfaqAliKashif <44152910+AshfaqAliKashif@users.noreply.github.com>
2024-02-23 15:38:23 +0500

    Initial commit

---
+++ skincare/main/views.py
@@ -0,0 +1,120 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home_page(request):
+    return render(request, 'index.html')
+def about(request):
+    return render(request, 'about.html')
+def service(request):
+    return render(request, 'services.html')
+def blog(request):
+    return render(request, 'blog.html')
+def blogdetails(request):
+    return render(request, 'blog-details.html')
+def contact(request):
+    return render(request, 'contact.html')
+def doctor(request):
+    return render(request, 'doctor.html')
+def main(request):
+    return render(request, 'main.html')
+def pricing(request):
+    return render(request, 'pricing.html')
+def base(request):
+    return render(request, 'base.html')
+def facial(request):
+    return render(request, 'service-pages/FacialProcedures/1.html')
+def HydrafacialSteps(request):
+    return render(request, 'service-pages/FacialProcedures/2.html')
+def Hydrafacialwhite(request):
+    return render(request, 'service-pages/FacialProcedures/3.html')
+def LuxuryHydra(request):
+    return render(request, 'service-pages/FacialProcedures/4.html')
+def Carbonfacial(request):
+    return render(request, 'service-pages/FacialProcedures/5.html')
+def Vampirefacial(request):
+    return render(request, 'service-pages/FacialProcedures/6.html')
+def Photofacial(request):
+    return render(request, 'service-pages/FacialProcedures/7.html')
+def bbglow(request):
+    return render(request, 'service-pages/SemiPermanentMakeup/1.html')
+def Eyelash(request):
+    return render(request, 'service-pages/SemiPermanentMakeup/2.html')
+def Microblading(request):
+    return render(request, 'service-pages/SemiPermanentMakeup/3.html')
+def LipTint(request):
+    return render(request, 'service-pages/SemiPermanentMakeup/4.html')
+def ThreadsLift(request):
+    return render(request, 'service-pages/ThreadsLift/1.html')
+def Face(request):
+    return render(request, 'service-pages/BrighteningPeelForGlassSkin/1.html')
+def FaceNeck(request):
+    return render(request, 'service-pages/BrighteningPeelForGlassSkin/2.html')
+def HandFeet(request):
+    return render(request, 'service-pages/BrighteningPeelForGlassSkin/3.html')
+def HIFU(request):
+    return render(request, 'service-pages/HIFU/1.html')
+def PRPF(request):
+    return render(request, 'service-pages/PRPFace/1.html')
+def PRPFMN(request):
+    return render(request, 'service-pages/PRPFace/2.html')
+def PRPFM(request):
+    return render(request, 'service-pages/PRPFace/3.html')
+def PRPFace(request):
+    return render(request, 'service-pages/PRPFace/4.html')
+def FPRCOM(request):
+    return render(request, 'service-pages/PRPFace/5.html')
+def PRPFCO(request):
+    return render(request, 'service-pages/PRPFace/6.html')
+def PRPFMeso(request):
+    return render(request, 'service-pages/PRPFace/7.html')
+def PRPBoTox(request):
+    return render(request, 'service-pages/PRPFace/8.html')
+def PRPFillers(request):
+    return render(request, 'service-pages/PRPFace/9.html')
+def PRPFProfhilo(request):
+    return render(request, 'service-pages/PRPFace/10.html')
+def PRPHS(request):
+    return render(request, 'service-pages/PRPHair/1.html')
+def PRPHM(request):
+    return render(request, 'service-pages/PRPHair/2.html')
+def PRPHB(request):
+    return render(request, 'service-pages/PRPHair/3.html')
+def WhiteningInjection(request):
+    return render(request, 'service-pages/WhiteningInjections/1.html')
+def SlimingDrips(request):
+    return render(request, 'service-pages/SlimmingDrips/1.html')
+def SlimingInjection(request):
+    return render(request, 'service-pages/SlimmingDrips/2.html')
+def LaserFF(request):
+    return render(request, 'service-pages/Lasers/1.html')
+def LaserFaceF(request):
+    return render(request, 'service-pages/Lasers/2.html')
+def LaserE(request):
+    return render(request, 'service-pages/Lasers/3.html')
+def LaserCA(request):
+    return render(request, 'service-pages/Lasers/4.html')
+def LaserB(request):
+    return render(request, 'service-pages/Lasers/5.html')
+def LaserUA(request):
+    return render(request, 'service-pages/Lasers/6.html')
+def LaserUL(request):
+    return render(request, 'service-pages/Lasers/7.html')
+def LaserFA(request):
+    return render(request, 'service-pages/Lasers/8.html')
+def LaserFL(request):
+    return render(request, 'service-pages/Lasers/9.html')
+def LaserHL(request):
+    return render(request, 'service-pages/Lasers/10.html')
+def LaserH(request):
+    return render(request, 'service-pages/Lasers/11.html')
+def LaserN(request):
+    return render(request, 'service-pages/Lasers/12.html')
+def LaserFBOne(request):
+    return render(request, 'service-pages/Lasers/13.html')
+def LaserFBTwo(request):
+    return render(request, 'service-pages/Lasers/14.html')
+def LaserFBThree(request):
+    return render(request, 'service-pages/Lasers/15.html')
+def LaserFBFour(request):
+    return render(request, 'service-pages/Lasers/16.html')
+
